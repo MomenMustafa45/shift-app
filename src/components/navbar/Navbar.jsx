1dedd6f2202e87d6ce113972d302e16b91492445
MomenMustafa45 <momenkara2@gmail.com>
2023-10-31 10:27:52 +0200

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import "./navbar.css";
+import { HashLink } from "react-router-hash-link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import navImg from "../../assests/logo shift-01 1.png";
+import navMediaScreensIcon from "../../assests/small-menuIcon.png";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+
+const navLinks = [
+  { name: "HOME", path: "/" },
+  { name: "ABOUT US", path: "/about" },
+  { name: "SERVICES", path: "/services" },
+  { name: "PARTENERS", path: "/parteners" },
+  { name: "REFERENCE", path: "/refernce" },
+  { name: "NEWS", path: "/news" },
+];
+
+const Navbar = () => {
+  const [toggleSmallScreenList, setToggleSmallScreenList] = useState(false);
+  const locationPathname = window.location.pathname;
+
+  return (
+    <header className="navbar-parent">
+      <img src={navImg} alt="this is nav img" />
+      {toggleSmallScreenList ? (
+        <FontAwesomeIcon
+          icon={faX}
+          style={{ cursor: "pointer", color: "#fff" }}
+          onClick={() => setToggleSmallScreenList(!toggleSmallScreenList)}
+        />
+      ) : (
+        <img
+          src={navMediaScreensIcon}
+          alt="this is icon"
+          className="small-screens-icon"
+          onClick={() => setToggleSmallScreenList(!toggleSmallScreenList)}
+        />
+      )}
+      <nav className="nav-links-container">
+        {navLinks.map((link) => (
+          <HashLink
+            to={link.path}
+            className={`${locationPathname === link.path ? "link-active" : ""}`}
+          >
+            {link.name}
+          </HashLink>
+        ))}
+      </nav>
+      <div className="header-btn">
+        <div>
+          <p>WHATSAPP</p>
+          <p>+966567913779</p>
+        </div>
+        <div className="header-icon-box">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </div>
+      </div>
+
+      {/* Small Screens Nav Links */}
+      {/* Small Screens Nav Links */}
+      <div
+        className="small-screens-list"
+        style={{
+          height: `${toggleSmallScreenList ? "calc(100vh - 70px)" : "0px"}`,
+        }}
+      >
+        <nav className="small-screens-links-container">
+          {navLinks.map((link) => (
+            <HashLink
+              to={link.path}
+              className={`${
+                locationPathname === link.path ? "small-screen-link-active" : ""
+              }`}
+            >
+              {link.name}
+            </HashLink>
+          ))}
+        </nav>
+        <div className="small-screens-header-icon">
+          <div>
+            <p>WHATSAPP</p>
+            <p>+966567913779</p>
+          </div>
+          <div className="header-icon-box">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </div>
+        </div>
+      </div>
+      {/* Small Screens Nav Links */}
+      {/* Small Screens Nav Links */}
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/landing-page/LandingPage.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../../components/navbar/Navbar";
+import "./landing-page.css";
+import landingVideo from "../../assests/landingVideo.mp4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+const LandingPage = () => {
+  return (
+    <main className="landing-parent">
+      <video src={landingVideo} autoPlay loop className="landing-video" />
+      <Navbar />
+
+      {/* Landing Content */}
+      {/* Landing Content */}
+      <div className="landing-content">
+        <h1>
+          Navigating <br /> <span>Solutions</span> Your Path <br /> To{" "}
+          <span>Success</span>
+        </h1>
+        <div className="landing-content-btn">
+          <p>Let's Go</p>
+          <div>
+            <FontAwesomeIcon icon={faArrowDown} />
+          </div>
+        </div>
+      </div>
+      {/* Landing Content */}
+      {/* Landing Content */}
+    </main>
+  );
+};
+
+export default LandingPage;
